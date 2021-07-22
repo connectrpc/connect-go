2cba1ed9e4a4c721c019a2664284f5f824d721a2
Akshay Shah <akshay@akshayshah.org>
2021-07-21 22:57:26 -0700

    Add utilities for encoding and decoding timeouts
    Add functions to work with gRPC's timeout format.

---
+++ timeout.go
@@ -0,0 +1,82 @@
+package rerpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	maxHours        = math.MaxInt64 / int64(time.Hour) // how many hours fit into a time.Duration?
+	maxTimeoutChars = 8                                // from gRPC protocol
+)
+
+var (
+	errNoTimeout = errors.New("no timeout")
+	timeoutUnits = []struct {
+		size time.Duration
+		char byte
+	}{
+		{time.Nanosecond, 'n'},
+		{time.Microsecond, 'u'},
+		{time.Millisecond, 'm'},
+		{time.Second, 'S'},
+		{time.Minute, 'M'},
+		{time.Hour, 'H'},
+	}
+	timeoutUnitLookup = make(map[byte]time.Duration)
+)
+
+func init() {
+	for _, pair := range timeoutUnits {
+		timeoutUnitLookup[pair.char] = pair.size
+	}
+}
+
+func parseTimeout(timeout string) (time.Duration, error) {
+	if timeout == "" {
+		return 0, errNoTimeout
+	}
+	unit, ok := timeoutUnitLookup[timeout[len(timeout)-1]]
+	if !ok {
+		return 0, fmt.Errorf("gRPC protocol error: timeout %q has invalid unit", timeout)
+	}
+	num, err := strconv.ParseInt(timeout[:len(timeout)-1], 10 /* base */, 64 /* bitsize */)
+	if err != nil || num < 0 {
+		return 0, fmt.Errorf("gRPC protocol error: invalid timeout %q", timeout)
+	}
+	if num > 99999999 { // timeout must be ASCII string of at most 8 digits
+		return 0, fmt.Errorf("gRPC protocol error: timeout %q is too long", timeout)
+	}
+	if num > maxHours {
+		// Timeout is effectively unbounded, so ignore it. The grpc-go
+		// implementation does the same thing.
+		return 0, errNoTimeout
+	}
+	return time.Duration(num) * unit, nil
+}
+
+func encodeTimeout(t time.Duration) (string, error) {
+	for _, pair := range timeoutUnits {
+		if digits := strconv.FormatInt(int64(t/pair.size), 10 /* base */); len(digits) < maxTimeoutChars {
+			return digits + string(pair.char), nil
+		}
+	}
+	return "", errNoTimeout // shouldn't reach here
+}
+
+func applyTimeout(r *http.Request) (*http.Request, func(), error) {
+	if to, err := parseTimeout(r.Header.Get("Grpc-Timeout")); err != nil && err != errNoTimeout {
+		return nil, nil, err
+	} else if err != nil {
+		ctx, cancel := context.WithTimeout(r.Context(), to)
+		r = r.WithContext(ctx)
+		return r, cancel, nil
+	}
+	// TODO: support a default timeout
+	return r, func() {}, nil
+}
