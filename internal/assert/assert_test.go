e713f8f0cf478966589666356392c1213c474fed
Sever Băneșiu <banesiu.sever@gmail.com>
2021-08-09 22:57:38 -0700

    Add more assert.Nil/NotNil tests (#6)

--- internal/assert/assert_test.go
+++ internal/assert/assert_test.go
@@ -18,11 +18,23 @@ func TestAssertions(t *testing.T) {
 
 	t.Run("nil", func(t *testing.T) {
 		Nil(t, nil, "")
-		var null *Pair
-		Nil(t, null, "")
-		NotNil(t, "foo", "")
-		NotNil(t, Pair{}, "")
+		Nil(t, (*chan int)(nil), "")
+		Nil(t, (*func())(nil), "")
+		Nil(t, (*map[int]int)(nil), "")
+		Nil(t, (*Pair)(nil), "")
+		Nil(t, (*[]int)(nil), "")
+
+		NotNil(t, make(chan int), "")
+		NotNil(t, func() {}, "")
+		NotNil(t, interface{}(1), "")
+		NotNil(t, make(map[int]int), "")
 		NotNil(t, &Pair{}, "")
+		NotNil(t, make([]int, 0), "")
+
+		NotNil(t, "foo", "")
+		NotNil(t, 0, "")
+		NotNil(t, false, "")
+		NotNil(t, Pair{}, "")
 	})
 
 	t.Run("zero", func(t *testing.T) {
