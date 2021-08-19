a87f178f13e7707e44ca542cff5d5d2c32b3425d
Akshay Shah <akshay@akshayshah.org>
2021-08-18 20:21:24 -0700

    Add benchmark for a REST server
    Just so we've got a point of comparison, add a hand-rolled REST-ish
    server and client to our benchmarks. The REST benchmark doesn't pool
    buffers, gzip readers, or gzip writers - I don't see much code out in
    the wild doing that either, so this feels pretty realistic.
    
    BenchmarkReRPC/unary-8             10618            112497 ns/op          145937 B/op        222 allocs/op
    BenchmarkREST/unary-8               4411            303664 ns/op         1744084 B/op        183 allocs/op
    BenchmarkGRPC/unary-8              34251             34231 ns/op           14435 B/op        174 allocs/op

--- internal/crosstest/cross_bench_test.go
+++ internal/crosstest/cross_bench_test.go
@@ -1,15 +1,20 @@
 package crosstest
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
+	"encoding/json"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"sync"
 	"testing"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/encoding/gzip"
+	grpcgzip "google.golang.org/grpc/encoding/gzip"
 
 	"github.com/rerpc/rerpc"
 	"github.com/rerpc/rerpc/internal/assert"
@@ -31,6 +36,99 @@ func BenchmarkReRPC(b *testing.B) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				_, _ = client.Ping(context.Background(), &crosspb.PingRequest{Number: 42})
+			}
+		})
+	})
+}
+
+func BenchmarkREST(b *testing.B) {
+	type ping struct {
+		Number int `json:"number"`
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		defer io.Copy(io.Discard, r.Body)
+		w.Header().Set("Content-Type", "application/json")
+		var body io.Reader = r.Body
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			gr, err := gzip.NewReader(body)
+			if err != nil {
+				b.Fatalf("get gzip reader: %v", err)
+			}
+			defer gr.Close()
+			body = gr
+		}
+		var out io.Writer = w
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gw := gzip.NewWriter(w)
+			defer gw.Close()
+			out = gw
+		}
+		raw, err := io.ReadAll(body)
+		if err != nil {
+			b.Fatalf("read body: %v", err)
+		}
+		var req ping
+		if err := json.Unmarshal(raw, &req); err != nil {
+			b.Fatalf("json unmarshal: %v", err)
+		}
+		bs, err := json.Marshal(&req)
+		if err != nil {
+			b.Fatalf("json marshal: %v", err)
+		}
+		out.Write(bs)
+	}
+
+	server := httptest.NewUnstartedServer(http.HandlerFunc(handler))
+	server.EnableHTTP2 = true
+	server.StartTLS()
+	defer server.Close()
+	b.ResetTimer()
+
+	b.Run("unary", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				rawRequestBody := bytes.NewBuffer(nil)
+				compressedRequestBody := gzip.NewWriter(rawRequestBody)
+				encoder := json.NewEncoder(compressedRequestBody)
+				if err := encoder.Encode(&ping{42}); err != nil {
+					b.Fatalf("marshal request: %v", err)
+				}
+				compressedRequestBody.Close()
+				request, err := http.NewRequestWithContext(
+					context.Background(),
+					http.MethodPost,
+					server.URL,
+					rawRequestBody,
+				)
+				if err != nil {
+					b.Fatalf("construct request: %v", err)
+				}
+				request.Header.Set("Content-Encoding", "gzip")
+				request.Header.Set("Accept-Encoding", "gzip")
+				request.Header.Set("Content-Type", "application/json")
+				res, err := server.Client().Do(request)
+				if err != nil {
+					b.Fatalf("do request: %v", err)
+				}
+				defer io.Copy(io.Discard, res.Body)
+				if res.StatusCode != http.StatusOK {
+					b.Fatalf("response status: %v", res.Status)
+				}
+				uncompressed, err := gzip.NewReader(res.Body)
+				if err != nil {
+					b.Fatalf("uncompress response: %v", err)
+				}
+				raw, err := io.ReadAll(uncompressed)
+				if err != nil {
+					b.Fatalf("read response: %v", err)
+				}
+				var got ping
+				if err := json.Unmarshal(raw, &got); err != nil {
+					b.Fatalf("unmarshal: %v", err)
+				}
 			}
 		})
 	})
@@ -63,7 +161,7 @@ func BenchmarkGRPC(b *testing.B) {
 	b.Run("unary", func(b *testing.B) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
-				_, _ = client.Ping(context.Background(), &crosspb.PingRequest{Number: 42}, grpc.UseCompressor(gzip.Name))
+				_, _ = client.Ping(context.Background(), &crosspb.PingRequest{Number: 42}, grpc.UseCompressor(grpcgzip.Name))
 			}
 		})
 	})
