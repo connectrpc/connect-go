f2e0f9c5e65ea6b5da91a38a09eadf1e4eedcb26
Akshay Shah <akshay@akshayshah.org>
2022-01-10 21:59:13 -0800

    sq into readme

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 // Package rerpc is a small RPC framework built on protocol buffers and
-// net/http. It's wire-compatible with gRPC and Twirp.
+// net/http. It's wire-compatible with gRPC, including support for streaming.
 //
 // This documentation is intended to explain each type and function in
-// isolation. For walkthroughs, comparisons to grpc-go and Twirp, and other
-// narrative docs, see https://rerpc.github.io.
+// isolation. For walkthroughs, comparisons to grpc-go, and other narrative
+// docs, see https://rerpc.github.io.
 package rerpc
