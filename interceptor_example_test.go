06860f0ec10487298122fd6f5dd881e01607896d
Akshay Shah <akshay@akshayshah.org>
2022-01-28 16:00:47 -0800

    Intercept -> Interceptors
    We have Compressor, so Interceptors seems better - we're mostly a
    Kingdom of Nouns.

--- interceptor_example_test.go
+++ interceptor_example_test.go
@@ -26,7 +26,7 @@ func ExampleInterceptor() {
 	client, err := pingrpc.NewPingServiceClient(
 		"https://invalid-test-url",
 		http.DefaultClient,
-		rerpc.Intercept(logProcedure, short),
+		rerpc.Interceptors(logProcedure, short),
 	)
 	if err != nil {
 		logger.Print("Error: ", err)
@@ -38,7 +38,7 @@ func ExampleInterceptor() {
 	// calling rerpc.ping.v1test.PingService/Ping
 }
 
-func ExampleIntercept() {
+func ExampleInterceptors() {
 	logger := log.New(os.Stdout, "" /* prefix */, 0 /* flags */)
 	outer := rerpc.UnaryInterceptorFunc(func(next rerpc.Func) rerpc.Func {
 		return rerpc.Func(func(ctx context.Context, req rerpc.AnyRequest) (rerpc.AnyResponse, error) {
@@ -62,7 +62,7 @@ func ExampleIntercept() {
 	client, err := pingrpc.NewPingServiceClient(
 		"https://invalid-test-url",
 		http.DefaultClient,
-		rerpc.Intercept(outer, inner, short),
+		rerpc.Interceptors(outer, inner, short),
 	)
 	if err != nil {
 		logger.Print("Error: ", err)
