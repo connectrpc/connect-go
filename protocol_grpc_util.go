44e34a90b955c0a5d7f66e5d90fbf0b344663b3a
Akshay Shah <akshay@akshayshah.org>
2022-02-02 22:08:08 -0800

    Add support for binary gRPC-Web
    Make handlers support gRPC-Web by default, and let users configure
    clients to use gRPC-Web with an option. This is such a small change from
    standard gRPC that it's easier to make it a configurable toggle on the
    gRPC protocol rather than a completely separate protocol.
    
    Note that we don't support the application/grpc-web-text family of
    protocols. They're only necessary for IE10, which even Microsoft doesn't
    support anymore: any client using the fetch API should use the binary
    protocol instead.
    
    Fixes #2

--- protocol_grpc_util.go
+++ protocol_grpc_util.go
@@ -2,15 +2,20 @@ package connect
 
 import (
 	"fmt"
+	"net/http"
 	"runtime"
+	"strconv"
 	"strings"
 
+	"github.com/bufconnect/connect/codec"
 	"github.com/bufconnect/connect/codec/protobuf"
 )
 
 const (
-	typeDefaultGRPC = "application/grpc"
-	typeGRPCPrefix  = typeDefaultGRPC + "+"
+	typeDefaultGRPC       = "application/grpc"
+	typeWebGRPC           = "application/grpc-web"
+	typeDefaultGRPCPrefix = typeDefaultGRPC + "+"
+	typeWebGRPCPrefix     = typeWebGRPC + "+"
 	// gRPC protocol uses "proto" instead of "protobuf"
 	grpcNameProto = "proto"
 )
@@ -21,27 +26,37 @@ func isCommaOrSpace(c rune) bool {
 	return c == ',' || c == ' '
 }
 
-func acceptPostValue(codecs roCodecs) string {
+func acceptPostValue(web bool, codecs roCodecs) string {
+	bare, prefix := typeDefaultGRPC, typeDefaultGRPCPrefix
+	if web {
+		bare, prefix = typeWebGRPC, typeWebGRPCPrefix
+	}
 	names := codecs.Names()
 	for i, n := range names {
 		if n == protobuf.NameBinary {
-			n = "proto"
+			n = grpcNameProto
 		}
-		names[i] = "application/grpc+" + n
+		names[i] = prefix + n
 	}
-	names = append(names, "application/grpc")
+	if codecs.Get(protobuf.NameBinary) != nil {
+		names = append(names, bare)
+	}
 	return strings.Join(names, ",")
 }
 
-func codecFromContentType(ctype string) string {
-	if ctype == typeDefaultGRPC {
+func codecFromContentType(web bool, ctype string) string {
+	if (!web && ctype == typeDefaultGRPC) || (web && ctype == typeWebGRPC) {
 		// implicitly protobuf
 		return protobuf.NameBinary
 	}
-	if !strings.HasPrefix(ctype, typeGRPCPrefix) {
+	prefix := typeDefaultGRPCPrefix
+	if web {
+		prefix = typeWebGRPCPrefix
+	}
+	if !strings.HasPrefix(ctype, prefix) {
 		return ""
 	}
-	name := strings.TrimPrefix(ctype, typeGRPCPrefix)
+	name := strings.TrimPrefix(ctype, prefix)
 	if name == grpcNameProto {
 		// normalize to our "protobuf"
 		return protobuf.NameBinary
@@ -49,10 +64,37 @@ func codecFromContentType(ctype string) string {
 	return name
 }
 
-func contentTypeFromCodecName(n string) string {
+func contentTypeFromCodecName(web bool, n string) string {
 	// translate back to gRPC's "proto"
 	if n == protobuf.NameBinary {
 		n = grpcNameProto
 	}
-	return typeGRPCPrefix + n
+	if web {
+		return typeWebGRPCPrefix + n
+	}
+	return typeDefaultGRPCPrefix + n
+}
+
+func grpcErrorToTrailer(trailer http.Header, protobuf codec.Codec, err error) error {
+	if CodeOf(err) == CodeOK { // safe for nil errors
+		trailer.Set("Grpc-Status", strconv.Itoa(int(CodeOK)))
+		trailer.Set("Grpc-Message", "")
+		trailer.Set("Grpc-Status-Details-Bin", "")
+		return nil
+	}
+	s, statusError := statusFromError(err)
+	if statusError != nil {
+		return statusError
+	}
+	code := strconv.Itoa(int(s.Code))
+	bin, err := protobuf.Marshal(s)
+	if err != nil {
+		trailer.Set("Grpc-Status", strconv.Itoa(int(CodeInternal)))
+		trailer.Set("Grpc-Message", percentEncode("error marshaling protobuf status with code "+code))
+		return Errorf(CodeInternal, "couldn't marshal protobuf status: %w", err)
+	}
+	trailer.Set("Grpc-Status", code)
+	trailer.Set("Grpc-Message", percentEncode(s.Message))
+	trailer.Set("Grpc-Status-Details-Bin", EncodeBinaryHeader(bin))
+	return nil
 }
