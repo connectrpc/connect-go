593a64f68aff43d06fff21f0d9e68d6297774418
Akshay Shah <akshay@akshayshah.org>
2022-02-15 23:51:06 -0800

    style: unary_interceptor.go

--- unary_interceptor.go
+++ unary_interceptor.go
@@ -4,7 +4,7 @@ import "context"
 
 // A UnaryInterceptorFunc is a simple Interceptor implementation that only
 // wraps unary RPCs. It has no effect on client, server, or bidirectional
-// streaming RPCs. See CallMetadata for an example.
+// streaming RPCs.
 type UnaryInterceptorFunc func(Func) Func
 
 // Wrap implements Interceptor by applying the interceptor function.
@@ -16,11 +16,11 @@ func (f UnaryInterceptorFunc) WrapContext(ctx context.Context) context.Context {
 }
 
 // WrapSender implements Interceptor with a no-op.
-func (f UnaryInterceptorFunc) WrapSender(_ context.Context, s Sender) Sender {
-	return s
+func (f UnaryInterceptorFunc) WrapSender(_ context.Context, sender Sender) Sender {
+	return sender
 }
 
 // WrapReceiver implements Interceptor with a no-op.
-func (f UnaryInterceptorFunc) WrapReceiver(_ context.Context, r Receiver) Receiver {
-	return r
+func (f UnaryInterceptorFunc) WrapReceiver(_ context.Context, receiver Receiver) Receiver {
+	return receiver
 }
