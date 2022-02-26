834fb187d2cd05a586669fc154e226926ddd4e4d
Akshay Shah <akshay@akshayshah.org>
2022-02-25 18:34:02 -0800

    Remove connecttest
    Pull the in-memory HTTP server into the `connect_test` package, which is
    the only place we're currently using it.

--- example_init_test.go
+++ example_init_test.go
@@ -1,13 +1,117 @@
 package connect_test
 
 import (
+	"context"
+	"errors"
+	"net"
 	"net/http"
+	"net/http/httptest"
+	"sync"
 
-	"github.com/bufbuild/connect/connecttest"
 	pingrpc "github.com/bufbuild/connect/internal/gen/proto/go-connect/connect/ping/v1test"
 )
 
-var examplePingServer *connecttest.Server
+var examplePingServer *inMemoryServer
+
+// inMemoryServer is an HTTP server that uses in-memory pipes instead of TCP.
+// It supports HTTP/2 and has TLS enabled.
+//
+// It's intended to be a preconfigured, faster alternative to the standard
+// library's httptest.inMemoryServer.
+type inMemoryServer struct {
+	server   *httptest.Server
+	listener *memoryListener
+}
+
+// newInMemoryServer constructs and starts an inMemoryServer.
+func newInMemoryServer(handler http.Handler) *inMemoryServer {
+	lis := &memoryListener{
+		conns:  make(chan net.Conn),
+		closed: make(chan struct{}),
+	}
+	server := httptest.NewUnstartedServer(handler)
+	server.Listener = lis
+	server.EnableHTTP2 = true
+	server.StartTLS()
+	return &inMemoryServer{
+		server:   server,
+		listener: lis,
+	}
+}
+
+// Client returns an HTTP client configured to trust the server's TLS
+// certificate and use HTTP/2 over an in-memory pipe. Automatic HTTP-level gzip
+// compression is disabled. It closes its idle connections when the server is
+// closed.
+func (s *inMemoryServer) Client() *http.Client {
+	client := s.server.Client()
+	if transport, ok := client.Transport.(*http.Transport); ok {
+		transport.DialContext = s.listener.DialContext
+		transport.DisableCompression = true
+	}
+	return client
+}
+
+// URL is the server's URL.
+func (s *inMemoryServer) URL() string {
+	return s.server.URL
+}
+
+// Close shuts down the server, blocking until all outstanding requests have
+// completed.
+func (s *inMemoryServer) Close() {
+	s.server.Close()
+}
+
+type memoryListener struct {
+	conns  chan net.Conn
+	once   sync.Once
+	closed chan struct{}
+}
+
+// Accept implements net.Listener.
+func (l *memoryListener) Accept() (net.Conn, error) {
+	select {
+	case conn := <-l.conns:
+		return conn, nil
+	case <-l.closed:
+		return nil, errors.New("listener closed")
+	}
+}
+
+// Close implements net.Listener.
+func (l *memoryListener) Close() error {
+	l.once.Do(func() {
+		close(l.closed)
+	})
+	return nil
+}
+
+// Addr implements net.Listener.
+func (l *memoryListener) Addr() net.Addr {
+	return &memoryAddr{}
+}
+
+// DialContext is the type expected by http.Transport.DialContext.
+func (l *memoryListener) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	select {
+	case <-l.closed:
+		return nil, errors.New("listener closed")
+	default:
+	}
+	server, client := net.Pipe()
+	l.conns <- server
+	return client, nil
+}
+
+type memoryAddr struct{}
+
+// Network implements net.Addr.
+func (*memoryAddr) Network() string { return "memory" }
+
+// String implements io.Stringer, returning a value that matches the
+// certificates used by net/http/httptest.
+func (*memoryAddr) String() string { return "example.com" }
 
 func init() {
 	// Generally, init functions are bad.
@@ -20,5 +124,5 @@ func init() {
 	// The least-awful option is to set up the server in init().
 	mux := http.NewServeMux()
 	mux.Handle(pingrpc.NewPingServiceHandler(pingServer{}))
-	examplePingServer = connecttest.NewServer(mux)
+	examplePingServer = newInMemoryServer(mux)
 }
