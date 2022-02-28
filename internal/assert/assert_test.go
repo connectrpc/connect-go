64e289c7e2592db34f304d18bd51a4907b5f7a5f
bufdev <bufdev-github@buf.build>
2022-02-28 14:04:10 -0500

    Reorder assert types and add package documentation

--- internal/assert/assert_test.go
+++ internal/assert/assert_test.go
@@ -6,10 +6,6 @@ import (
 	"testing"
 )
 
-type Pair struct {
-	First, Second int
-}
-
 func TestAssertions(t *testing.T) {
 	t.Run("equal", func(t *testing.T) {
 		Equal(t, 1, 1, "%v aren't equal", Diff(), Fmt("ints"))
@@ -21,28 +17,28 @@ func TestAssertions(t *testing.T) {
 		Nil(t, (*chan int)(nil), "")
 		Nil(t, (*func())(nil), "")
 		Nil(t, (*map[int]int)(nil), "")
-		Nil(t, (*Pair)(nil), "")
+		Nil(t, (*pair)(nil), "")
 		Nil(t, (*[]int)(nil), "")
 
 		NotNil(t, make(chan int), "")
 		NotNil(t, func() {}, "")
 		NotNil(t, any(1), "")
 		NotNil(t, make(map[int]int), "")
-		NotNil(t, &Pair{}, "")
+		NotNil(t, &pair{}, "")
 		NotNil(t, make([]int, 0), "")
 
 		NotNil(t, "foo", "")
 		NotNil(t, 0, "")
 		NotNil(t, false, "")
-		NotNil(t, Pair{}, "")
+		NotNil(t, pair{}, "")
 	})
 
 	t.Run("zero", func(t *testing.T) {
 		var n *int
 		Zero(t, n, "")
-		var pair Pair
-		Zero(t, pair, "")
-		var null *Pair
+		var p pair
+		Zero(t, p, "")
+		var null *pair
 		Zero(t, null, "")
 		var s []int
 		Zero(t, s, "")
@@ -58,8 +54,8 @@ func TestAssertions(t *testing.T) {
 
 	t.Run("unexported fields", func(t *testing.T) {
 		// Two pairs differ only in an unexported field.
-		p1 := Pair{1, 2}
-		p2 := Pair{1, 3}
+		p1 := pair{1, 2}
+		p2 := pair{1, 3}
 		NotEqual(t, p1, p2, "")
 	})
 
@@ -70,4 +66,8 @@ func TestAssertions(t *testing.T) {
 	t.Run("panics", func(t *testing.T) {
 		Panics(t, func() { panic("testing") }, "")
 	})
+}
+
+type pair struct {
+	First, Second int
 }
