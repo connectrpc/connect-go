c07937f4dd3ff756adce1d2ff9b9cbb51a6ba384
Akshay Shah <akshay@akshayshah.org>
2022-02-28 22:26:27 -0800

    Switch to makego
    Convert the build to use makego.

---
+++ make/go/dep_protoc_gen_go.mk
@@ -0,0 +1,24 @@
+# Managed by makego. DO NOT EDIT.
+
+# Must be set
+$(call _assert_var,MAKEGO)
+$(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _assert_var,CACHE_VERSIONS)
+$(call _assert_var,CACHE_BIN)
+
+# Settable
+# https://github.com/protocolbuffers/protobuf-go/releases 20210628 checked 20210210
+PROTOC_GEN_GO_VERSION ?= v1.27.1
+
+GO_GET_PKGS := $(GO_GET_PKGS) \
+	google.golang.org/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
+
+PROTOC_GEN_GO := $(CACHE_VERSIONS)/protoc-gen-go/$(PROTOC_GEN_GO_VERSION)
+$(PROTOC_GEN_GO):
+	@rm -f $(CACHE_BIN)/protoc-gen-go
+	GOBIN=$(CACHE_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
+	@rm -rf $(dir $(PROTOC_GEN_GO))
+	@mkdir -p $(dir $(PROTOC_GEN_GO))
+	@touch $(PROTOC_GEN_GO)
+
+dockerdeps:: $(PROTOC_GEN_GO)
