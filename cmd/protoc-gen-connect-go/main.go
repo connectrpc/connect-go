a75807b0a480085a7af1eae251fa56a5af3c028d
Akshay Shah <akshay@akshayshah.org>
2022-03-01 15:16:37 -0800

    Add licenses

--- cmd/protoc-gen-connect-go/main.go
+++ cmd/protoc-gen-connect-go/main.go
@@ -1,3 +1,17 @@
+// Copyright 2021-2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // protoc-gen-connect-go is a plugin for the protocol buffer compiler that
 // generates Go code. To use it, build this program and make it available on
 // your PATH as protoc-gen-connect-go.
