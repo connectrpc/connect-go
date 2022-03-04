5c682705819575e93ee28441aff39d6ff9ab25c4
bufdev <4228796+bufdev@users.noreply.github.com>
2022-03-04 12:24:24 -0500

    Move errorTranslatingSender/Receiver into protocol.go (#120)

--- protocol.go
+++ protocol.go
@@ -116,3 +116,44 @@ type protocolClient interface {
 	// methods return before the Receiver's Receive or Close methods are called.
 	NewStream(context.Context, http.Header) (Sender, Receiver)
 }
+
+// errorTranslatingSender wraps a Sender to ensure that we always return coded
+// errors to clients and write coded errors to the network.
+//
+// This is used in protocol implementations.
+type errorTranslatingSender struct {
+	Sender
+
+	toWire   func(error) error
+	fromWire func(error) error
+}
+
+func (s *errorTranslatingSender) Send(msg any) error {
+	return s.fromWire(s.Sender.Send(msg))
+}
+
+func (s *errorTranslatingSender) Close(err error) error {
+	sendErr := s.Sender.Close(s.toWire(err))
+	return s.fromWire(sendErr)
+}
+
+// errorTranslatingReceiver wraps a Receiver to make sure that we always return
+// coded errors from clients.
+//
+// This is used in protocol implementations.
+type errorTranslatingReceiver struct {
+	Receiver
+
+	fromWire func(error) error
+}
+
+func (r *errorTranslatingReceiver) Receive(msg any) error {
+	if err := r.Receiver.Receive(msg); err != nil {
+		return r.fromWire(err)
+	}
+	return nil
+}
+
+func (r *errorTranslatingReceiver) Close() error {
+	return r.fromWire(r.Receiver.Close())
+}
