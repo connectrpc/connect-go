096a29a4d7fe2ea92af01ad5c2d68aa4affe0614
bufdev <bufdev-github@buf.build>
2022-03-29 18:26:05 -0400

    Slight cleanup

--- cmd/protoc-gen-connect-go/main.go
+++ cmd/protoc-gen-connect-go/main.go
@@ -63,7 +63,7 @@ const (
 	generatedFilenameExtension = ".connect.go"
 	generatedPackageSuffix     = "connect"
 
-	usage = "See https://docs.bufconnect.com for how to use this plugin.\n\nFlags:\n  -h, --help\tPrint this help.\n      --version\tPrint the version and exit.\n"
+	usage = "See https://docs.bufconnect.com for how to use this plugin.\n\nFlags:\n  -h, --help\tPrint this help and exit.\n      --version\tPrint the version and exit.\n"
 
 	commentWidth = 97 // leave room for "// "
 )
