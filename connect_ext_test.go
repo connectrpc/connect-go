a6916bd558d8f51198723c0a03d9933172fe6717
Philip K. Warren <pwarren@buf.build>
2022-06-20 09:58:16 -0400

    sleep longer for Windows

--- connect_ext_test.go
+++ connect_ext_test.go
@@ -95,7 +95,7 @@ func TestServer(t *testing.T) {
 		t.Run("ping_timout", func(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
 			defer cancel()
-			time.Sleep(time.Millisecond)
+			time.Sleep(2 * time.Millisecond)
 			request := connect.NewRequest(&pingv1.PingRequest{})
 			request.Header().Set(clientHeader, headerValue)
 			_, err := client.Ping(ctx, request)
@@ -175,7 +175,7 @@ func TestServer(t *testing.T) {
 		t.Run("count_up_timeout", func(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
 			defer cancel()
-			time.Sleep(time.Millisecond)
+			time.Sleep(2 * time.Millisecond)
 			_, err := client.CountUp(ctx, connect.NewRequest(&pingv1.CountUpRequest{Number: 1}))
 			assert.NotNil(t, err)
 			assert.Equal(t, connect.CodeOf(err), connect.CodeDeadlineExceeded)
