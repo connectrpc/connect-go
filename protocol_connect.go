bc83d1799e36fcb74a30dc46ee15ffe0677f78a3
Akshay Shah <akshayjshah@users.noreply.github.com>
2022-11-17 21:13:21 -0800

    Add RPC protocol to Peer (#394)
    Currently, observability interceptors must parse the `Content-Type`
    header to determine which RPC protocol is in use. This isn't awful, but
    it's so easy for `connect-go` to expose directly that we might as well.
    
    I chose to use our own strings rather than repurposing OpenTelemetry's
    semantic conventions here. The OTel strings are less nice in this
    context, and the packages are all unstable and subject to change.
    @joshcarp, LMK if this seems like a bad tradeoff to you.

--- protocol_connect.go
+++ protocol_connect.go
@@ -151,7 +151,10 @@ func (h *connectHandler) NewConn(
 	codec := h.Codecs.Get(codecName) // handler.go guarantees this is not nil
 
 	var conn handlerConnCloser
-	peer := Peer{Addr: request.RemoteAddr}
+	peer := Peer{
+		Addr:     request.RemoteAddr,
+		Protocol: ProtocolConnect,
+	}
 	if h.Spec.StreamType == StreamTypeUnary {
 		conn = &connectUnaryHandlerConn{
 			spec:           h.Spec,
@@ -221,7 +224,7 @@ type connectClient struct {
 }
 
 func (c *connectClient) Peer() Peer {
-	return newPeerFromURL(c.URL)
+	return newPeerFromURL(c.URL, ProtocolConnect)
 }
 
 func (c *connectClient) WriteRequestHeader(streamType StreamType, header http.Header) {
