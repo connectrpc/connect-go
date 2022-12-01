852ee1fb2e7600b4ead65d14415aa4e678a946f5
Josh Humphries <2035234+jhump@users.noreply.github.com>
2022-12-01 15:03:44 -0500

    review feedback: document behavior in the face of an Any; provide hyper-links to Any

--- error.go
+++ error.go
@@ -44,8 +44,9 @@ type ErrorDetail struct {
 	wireJSON string // preserve human-readable JSON
 }
 
-// NewErrorDetail constructs a new error detail. This returns an error
-// if msg cannot be marshalled into an Any message.
+// NewErrorDetail constructs a new error detail. If msg is an *[anypb.Any] then
+// it is used as is. Otherwise, it is first marshaleled into an *[anypb.Any]
+// value. This returns an error if msg cannot be marshalled.
 func NewErrorDetail(msg proto.Message) (*ErrorDetail, error) {
 	// If it's already an Any, don't wrap it inside another.
 	if pb, ok := msg.(*anypb.Any); ok {
