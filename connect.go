2052cb9f61c586fc6220ef56bae3872469d42925
Akshay Shah <akshay@akshayshah.org>
2023-07-26 09:53:59 -0700

    Back to development

--- connect.go
+++ connect.go
@@ -33,7 +33,7 @@ import (
 )
 
 // Version is the semantic version of the connect module.
-const Version = "1.10.0"
+const Version = "1.11.0-dev"
 
 // These constants are used in compile-time handshakes with connect's generated
 // code.
