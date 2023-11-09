67dceff8ffdf2bb79c961e800ccc98cf68d9d5e3
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2023-11-09 11:04:56 -0500

    Fix flaky test failure in concurrent streams test (#624)
    Using a fixed level of parallelism means the test takes too long
    when run on a machine with fewer resources/CPUs. So it would
    occasionally encounter a timeout-related issue in CI. Fixed by
    tying the level of parallelism to GOMAXPROCS (whose default
    value comes from the number of CPIs).

--- connect_ext_test.go
+++ connect_ext_test.go
@@ -26,6 +26,7 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"runtime"
 	"strings"
 	"sync"
 	"testing"
@@ -475,7 +476,7 @@ func TestConcurrentStreams(t *testing.T) {
 	server := memhttptest.NewServer(t, mux)
 	var done, start sync.WaitGroup
 	start.Add(1)
-	for i := 0; i < 100; i++ {
+	for i := 0; i < runtime.GOMAXPROCS(0)*8; i++ {
 		done.Add(1)
 		go func() {
 			defer done.Done()
