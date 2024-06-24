381e5d453364615fcb43b59e38c5378b03447436
John Chadwick <jchadwick@buf.build>
2024-06-24 12:36:57 -0400

    Enable protoc-gen-connect-go usage with editions
    Signed-off-by: John Chadwick <jchadwick@buf.build>

--- cmd/protoc-gen-connect-go/main.go
+++ cmd/protoc-gen-connect-go/main.go
@@ -91,7 +91,9 @@ func main() {
 	}
 	protogen.Options{}.Run(
 		func(plugin *protogen.Plugin) error {
-			plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+			plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
+			plugin.SupportedEditionsMinimum = descriptorpb.Edition_EDITION_PROTO2
+			plugin.SupportedEditionsMaximum = descriptorpb.Edition_EDITION_2023
 			for _, file := range plugin.Files {
 				if file.Generate {
 					generate(plugin, file)
